16c7b80645e7ce9a1fbc7b2fc38812e453253acd
JaswanthP6878 <klucse2000039034@gmail.com>
2023-07-03 12:03:35 +0530

    added footer and header templates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { ItemdetailsComponent } from './itemdetails/itemdetails.component';
+import { OrderlistComponent } from './orderlist/orderlist.component';
+import { UpdateprofileComponent } from './updateprofile/updateprofile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegistrationComponent},
+  {path: 'itemdetails', component: ItemdetailsComponent},
+  {path: 'orderlist', component: OrderlistComponent},
+  {path: "updateprofile", component: UpdateprofileComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
